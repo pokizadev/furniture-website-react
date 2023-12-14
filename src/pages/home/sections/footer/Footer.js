ab8385f2b7952d740c572f37868bbc51b75f483e
PokizaSdv <pokizasdv@gmail.com>
2023-12-13 23:44:44 -0500

    Create Mobile Navigation

--- src/pages/home/sections/footer/Footer.js
+++ src/pages/home/sections/footer/Footer.js
@@ -5,32 +5,32 @@ import { footerColumns } from "./data";
 const Footer = () => {
     return (
         <footer id="footer">
-            <div class="container">
-                <div class="footer__content">
-                    <div class="footer__brand footer-column">
+            <div className="container">
+                <div className="footer__content">
+                    <div className="footer__brand footer-column">
                         <img src={logo} alt="Logo"/>
-                        <p class="text-lg">
+                        <p className="text-lg">
                             We make furniture with love and passions. No doubt,
                             really.
                         </p>
-                        <p class="text-lg">© TuranTech Inc.</p>
+                        <p className="text-lg">© TuranTech Inc.</p>
                     </div>
                     {footerColumns.map((column, idx) => {
                         return (
                             <div
-                                class="footer__contact footer-column"
+                                className="footer__contact footer-column"
                                 key={idx}
                             >
-                                <h4 class="footer__heading uppercase-text-2">
+                                <h4 className="footer__heading uppercase-text-2">
                                     {column.heading}
                                 </h4>
-                                <a href={column.link1.link} class="text-lg">
+                                <a href={column.link1.link} className="text-lg">
                                     {column.link1.text}
                                 </a>
-                                <a href={column.link2.link} class="text-lg">
+                                <a href={column.link2.link} className="text-lg">
                                     {column.link2.text}
                                 </a>
-                                <a href={column.link3.link} class="text-lg">
+                                <a href={column.link3.link} className="text-lg">
                                     {column.link3.text}
                                 </a>
                             </div>
--- src/pages/home/sections/header/Header.js
+++ src/pages/home/sections/header/Header.js
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../../../assets/Whiter..png";
 import mobileNav from "../../../../assets/menu 1.svg";
+import React from "react";
 
 const links = [
     { text: "Products", link: "http://google.com" },
@@ -9,47 +10,116 @@ const links = [
     { text: "Support", link: "http://google.com" }
 ];
 
-const Header = () => {
-    return (
-        <header id="header">
-            <div className="container">
-                <img src={logo} alt="" className="logo" />
-                <nav className="navigation">
-                    <ul className="navigation__links">
-                        {links.map((link, idx) => {
-                            return (
-                                <li key={idx}>
-                                    <a href={link.link}>{link.text}</a>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    <div className="signIn_wrapper">
-                        <a className="signIn" href="www.facebook.com">
-                            Sign in
-                        </a>
-                    </div>
-                </nav>
-                <img
-                    src={mobileNav}
-                    alt="mobile-navigation"
-                    id="mobile-nav-icon"
-                />
-                <nav className="mobile-nav">
-                    <span className="close-icon">&times;</span>
-                    <ul className="mobile-nav_link">
-                        {links.map((link, idx) => {
-                            return (
-                                <li key={idx}>
-                                    <a href={link.link}>link.text</a>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
-};
+// const Header = () => {
+//     return (
+//         <header id="header">
+//             <div className="container">
+//                 <img src={logo} alt="" className="logo" />
+//                 <nav className="navigation">
+//                     <ul className="navigation__links">
+//                         {links.map((link, idx) => {
+//                             return (
+//                                 <li key={idx}>
+//                                     <a href={link.link}>{link.text}</a>
+//                                 </li>
+//                             );
+//                         })}
+//                     </ul>
+//                     <div className="signIn_wrapper">
+//                         <a className="signIn" href="www.facebook.com">
+//                             Sign in
+//                         </a>
+//                     </div>
+//                 </nav>
+//                 <img
+//                     src={mobileNav}
+//                     alt="mobile-navigation"
+//                     id="mobile-nav-icon"
+//                 />
+//                 <nav className="mobile-nav">
+//                     <span className="close-icon">&times;</span>
+//                     <ul className="mobile-nav_link">
+//                         {links.map((link, idx) => {
+//                             return (
+//                                 <li key={idx}>
+//                                     <a href={link.link}>link.text</a>
+//                                 </li>
+//                             );
+//                         })}
+//                     </ul>
+//                 </nav>
+//             </div>
+//         </header>
+//     );
+// };
+// export { Header };
 
-export { Header };
+export class Header extends React.Component {
+    state = {
+        showMenu: false
+    };
+
+    openMenu = () => {
+        this.setState({
+            showMenu: true
+        });
+    };
+
+    closeMenu = () => {
+        this.setState({
+            showMenu: false
+        });
+    };
+
+    render() {
+        return (
+            <header id="header">
+                <div className="container">
+                    <img src={logo} alt="" className="logo" />
+
+                    <nav className="navigation">
+                        <ul className="navigation__links">
+                            {links.map((link, idx) => {
+                                return (
+                                    <li key={idx}>
+                                        <a href={link.link}>{link.text}</a>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                        <div className="signIn_wrapper">
+                            <a className="signIn" href="www.facebook.com">
+                                Sign in
+                            </a>
+                        </div>
+                    </nav>
+                    <img
+                        onClick={this.openMenu}
+                        src={mobileNav}
+                        alt="mobile-navigation"
+                        id="mobile-nav-icon"
+                    />
+                    {this.state.showMenu ? (
+                        <nav className="mobile-nav">
+                            <span
+                                className="close-icon"
+                                onClick={this.closeMenu}
+                            >
+                                X
+                            </span>
+                            <ul className="mobile-nav_link">
+                                {links.map((link, idx) => {
+                                    return (
+                                        <li key={idx}>
+                                            <a href={link.link}>{link.text}</a>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </nav>
+                    ) : null}
+                </div>
+            </header>
+        );
+    }
+}
